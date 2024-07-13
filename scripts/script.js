fcc494e93ce9913fda4efb5485b7f50cc363c9d6
mohammad reza <mrghkh12@gmail.com>
2024-07-13 11:47:52 +0330

    feat(script.js): write create note function & set event for btn

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,15 @@ const $ = document;
 const createNoteBtn = $.querySelector(".createBtn")
 const notesContainer = $.querySelector('.notes-list')
 
-console.log(createNoteBtn , notesContainer);
+const createNote = () =>{
+    let noteElem = $.createElement('p')
+    noteElem.classList.add('notesInput')
+    noteElem.setAttribute('contenteditable' , 'true')
+
+    let delNoteElem = $.createElement('img')
+    delNoteElem.setAttribute('src' , 'images/delete.png')
+    delNoteElem.classList.add('del-icone')
+
+    notesContainer.appendChild(noteElem).appendChild(delNoteElem)
+}
+createNoteBtn.addEventListener('click' , createNote)
